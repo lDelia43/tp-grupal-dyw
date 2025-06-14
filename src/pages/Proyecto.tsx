342883bbd33b012aa115046a51c7082de26a5541
blammertyn <blammertyn@uade.edu.ar>
2025-06-14 16:05:03 -0300

    actualizacion proyecto

--- src/pages/Proyecto.tsx
+++ src/pages/Proyecto.tsx
@@ -3,8 +3,12 @@ export default function Proyecto() {
     <section className="text-center mt-20">
       <h1 className="text-4xl font-bold text-primary mb-6">Sitio en Construcción</h1>
       <p className="text-lg text-neutral-dark">
-        Esta sección será desarrollada y actualizada en la entrega final del proyecto.
+        "Este sitio te invita a explorar Aqualandia, el parque acuático ideal para disfrutar en familia o con amigos. Encontrá toda la información sobre atracciones, horarios, precios y eventos especiales. ¡Sumergite en la diversión y planificá tu próxima aventura acuática con nosotros!"
       </p>
+      <a
+        href = "#VIDEO EXPLICATIVO"
+      >
+      </a>  
     </section>
   )
 }
